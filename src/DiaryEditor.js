7f3b29c761cc30e7b7de8d043274c69305a81fbd
pkwesst <pkwesst@gmail.com>
2022-05-07 01:16:01 +0900

    author, content

---
+++ src/DiaryEditor.js
@@ -0,0 +1,73 @@
+import { useState } from "react";
+
+const DiaryEditor = () => {
+  const [state, setState] = useState({
+    author: "",
+    content: "",
+    emotion: 1,
+  });
+
+  const handleChangeState = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = () => {
+    if (state.author.length < 1) {
+      alert("작성자는 최소 1글자 이상 입력해주세요.");
+      return;
+    }
+
+    if (state.content.length < 5) {
+      alert("일기 본문은 최소 5글자 이상 입력해주세요");
+      return;
+    }
+
+    alert("저장 성공");
+  };
+
+  return (
+    <div className="DiaryEditor">
+      <h2>오늘의 일기</h2>
+      <div>
+        <input
+          name="author"
+          value={state.author}
+          onChange={handleChangeState}
+          placeholder="작성자"
+          type="text"
+        />
+      </div>
+      <div>
+        <textarea
+          name="content"
+          value={state.content}
+          onChange={handleChangeState}
+          placeholder="일기"
+          type="text"
+        />
+      </div>
+      <div>
+        <span>오늘의 감정점수 : </span>
+        <select
+          name="emotion"
+          value={state.emotion}
+          onChange={handleChangeState}
+        >
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+      </div>
+      <div>
+        <button onClick={handleSubmit}>일기 저장하기</button>
+      </div>
+    </div>
+  );
+};
+
+export default DiaryEditor;

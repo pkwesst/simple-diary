2d0abb948c5b8689752069f2b6df709a17e792ab
pkwesst <pkwesst@gmail.com>
2022-05-07 12:39:20 +0900

    create diaryitem

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import "./App.css";
+import React, { useState, useEffect } from "react";
 import DiaryEditor from "./DiaryEditor";
+import DiaryList from "./DiaryList";
+import "./App.css";
 
-function App() {
+const dummyList = [
+  {
+    id: 1,
+    author: "홍길동",
+    content: "안녕 1",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "윤선도",
+    content: "안녕 2",
+    emotion: 3,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "정철",
+    content: "안녕 3",
+    emotion: 4,
+    created_date: new Date().getTime(),
+  },
+];
+
+const App = () => {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
-}
+};
 
 export default App;
